043feef9916b24b34629661e48e382bf13f4055a
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-09-10 23:25:33 -0400

    Improve burst

--- build/bundle.js
+++ build/bundle.js
@@ -2,34 +2,23 @@ var debounce = false;
 function setup() {
     var cvs = createCanvas(1024, 768);
     cvs.elt.id = "mainCanvas";
-    noFill();
 }
 function draw() {
     if (mouseIsPressed && !debounce) {
         debounce = true;
-        var numRays = Math.max(3, randomGaussian(7, 2));
+        var numRays = random(3, 6);
         for (var i = 0; i < numRays; i++) {
             var angle = random(0, 360);
             var vec = p5.Vector.fromAngle(radians(angle));
-            var numCircles = Math.max(1, randomGaussian(5, 2));
+            var slope = vec.y / vec.x;
+            var numCircles = random(3, 8);
             for (var i_1 = 0; i_1 < numCircles; i_1++) {
-                var size = random(5, 30);
-                var x = mouseX + (vec.x * Math.abs(randomGaussian(0, 50)));
-                var y = mouseY + (vec.y * Math.abs(randomGaussian(0, 50)));
+                var size = random(5, 15);
+                var y = mouseY + randomGaussian(0, 25);
+                var x = slope * (y - mouseY) + mouseX;
                 ellipse(x, y, size, size);
             }
         }
-        setTimeout(function () { return debounce = false; }, 100);
-    }
-}
-function circleLine(x1, y1, x2, y2) {
-    var numCircles = Math.max(0, randomGaussian(5, 2));
-    for (var i = 0; i < numCircles; i++) {
-        var size = random(5, 30);
-        var slope = (y2 - y1) / (x2 - x1);
-        var length_1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        var x = x1 + ((length_1 / (i + 1)) * slope);
-        var y = y1 + ((length_1 / (i + 1)) * slope);
-        ellipse(x, y, size, size);
+        setTimeout(function () { return debounce = false; }, 200);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,24 @@ let debounce = false;
 function setup() {
     const cvs = createCanvas(1024, 768);
     cvs.elt.id = "mainCanvas";
-    noFill();
 }
 
 function draw() {
     if (mouseIsPressed && !debounce) {
         debounce = true;
-        const numRays = Math.max(3, randomGaussian(7, 2));
+        const numRays = random(3, 6);
         for (let i = 0; i < numRays; i++) {
             const angle = random(0, 360);
             const vec = p5.Vector.fromAngle(radians(angle));
-            const numCircles = Math.max(1, randomGaussian(5, 2));
+            const slope = vec.y / vec.x;
+            const numCircles = random(3, 8);
             for (let i = 0; i < numCircles; i++) {
-                const size = random(5, 30);
-                const x = mouseX + (vec.x * Math.abs(randomGaussian(0, 50)));
-                const y = mouseY + (vec.y * Math.abs(randomGaussian(0, 50)));
+                const size = random(5, 15);
+                const y = mouseY + randomGaussian(0, 25);
+                const x = slope * (y - mouseY) + mouseX;
                 ellipse(x, y, size, size);
             }
         }
-        setTimeout(() => debounce = false, 100);
+        setTimeout(() => debounce = false, 200);
     }
 }
